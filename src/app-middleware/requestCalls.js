420fafef3638b2fac4de6751f412c6d9a55bd683
Vitor Viana <masterviana@gmail.com>
2022-12-12 19:57:10 +0000

    fix log

--- src/app-middleware/requestCalls.js
+++ src/app-middleware/requestCalls.js
@@ -1,24 +1,37 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
-const logDebug = require('$core-services/logFunctionFactory').getDebugLogger()
+const { Router } = require("express");
+const logDebug = require("$core-services/logFunctionFactory").getDebugLogger();
 
 function requestCallLogHandler(request, response, next) {
-  let body = request.method  == 'GET' ? request.query : request.body;
-  if(process.env.LOGGER_LEVEL.toLocaleLowerCase() == 'debug'){
-    logDebug('[CALL_ROUTE] [',request.originalUrl,'] ', ' METHOD [', request.method , '] BODY ', body);
+  let body = request.method == "GET" ? request.query : request.body;
+
+  if (
+    process.env.LOGGER_LEVEL &&
+    process.env.LOGGER_LEVEL.toLocaleLowerCase() == "debug"
+  ) {
+    logDebug(
+      "[CALL_ROUTE] [",
+      request.originalUrl,
+      "] ",
+      " METHOD [",
+      request.method,
+      "] BODY ",
+      body
+    );
   }
-   next();
+  next();
 }
 
-function AddCorsRules ( req, res, next) {
+function AddCorsRules(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
-};
+}
 
 module.exports = function requestCallLog() {
-  return Router()
-    .use(AddCorsRules)
-    .use(requestCallLogHandler);
+  return Router().use(AddCorsRules).use(requestCallLogHandler);
 };
