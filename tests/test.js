58fee8b220ab6ca3ee2e85874bf8d77863e71f5d
Vitor Viana <masterviana@gmail.com>
2022-12-15 15:17:42 +0000

    update google

---
+++ tests/test.js
@@ -0,0 +1,32 @@
+let { google } = require("googleapis");
+
+const GOOGLE_CLIENT_ID = "";
+const GOOGLE_CLIENT_SECRET = "";
+
+const oauth2Client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  /*
+   * This is where Google will redirect the user after they
+   * give permission to your application
+   */
+  "https://sdk-iframe.herokuapp.com"
+);
+
+function getGoogleAuthURL() {
+  /*
+   * Generate a url that asks permissions to the user's email and profile
+   */
+  const scopes = [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ];
+
+  return oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes, // If you only need one scope you can pass it as string
+  });
+}
+
+console.log("Get Google : ", getGoogleAuthURL());
